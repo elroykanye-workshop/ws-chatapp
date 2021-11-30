d14a96913d0ed4c96b988e85dd7f1b662ec78ef5
elroykanye <elroykanye@gmail.com>
2021-11-30 23:53:59 +0100

    initial commit completed backend : added ./chatapp-backend/src/test/java/com/elroykanye/wschatapp/WsChatappApplicationTests.java

---
+++ chatapp-backend/src/test/java/com/elroykanye/wschatapp/WsChatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elroykanye.wschatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsChatappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
