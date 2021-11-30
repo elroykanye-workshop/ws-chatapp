cc12ce914489aab3b40122a6447af766098bd9b1
elroykanye <elroykanye@gmail.com>
2021-11-30 23:53:58 +0100

    initial commit completed backend : added ./chatapp-backend/src/main/java/com/elroykanye/wschatapp/WsChatappApplication.java

---
+++ chatapp-backend/src/main/java/com/elroykanye/wschatapp/WsChatappApplication.java
@@ -0,0 +1,13 @@
+package com.elroykanye.wschatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsChatappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsChatappApplication.class, args);
+    }
+
+}
