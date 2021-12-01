ee87bc71cf9e8167a5bb0d1e47b71c8d544abaf5
elroykanye <elroykanye@gmail.com>
2021-12-01 06:03:27 +0100

    added design for chat content

--- chatapp-frontend/src/app/app.module.ts
+++ chatapp-frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChatComponent } from './component/chat/chat.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
